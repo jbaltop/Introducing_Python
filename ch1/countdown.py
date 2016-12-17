59189b6f53ecd8ef5766c34e5b81647e5e6da425
Jay Park <jaypark@innods.com>
2016-12-17 22:20:06 +0900

    chapter 1 examples

---
+++ ch1/countdown.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3
+
+import time
+
+for countdown in 5, 4, 3, 2, 1, "hey!":
+	time.sleep(1)
+	print("--------------")
+	print(countdown)
---
+++ ch1/list.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+cliches = [
+	"At the end of the day",
+	"Having said that",
+	"The fact of the matter is",
+	"Be that as it may",
+	"The bottom line is",
+	"If you will",
+	]
+for count in range(6):
+	print(cliches[count])
